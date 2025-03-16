15455b6e5ae2bf59ba7495407300b314e16369c5
Cameron M <cameronmdev1@gmail.com>
2025-03-16 13:05:04 -0700

    fixed db connection to allow local infiles

--- project.py
+++ project.py
@@ -2,7 +2,13 @@ import mysql.connector
 from setup_db import initialize_db, populate_db
 
 def open_db_connection():
-    return mysql.connector.connect(user='test', password='password', database='cs122a')
+    return mysql.connector.connect(
+        host="localhost",
+        user="test",
+        password="password",
+        database="cs122a",
+        allow_local_infile=True
+    )
 
 if __name__ == "__main__":
     db_connection = open_db_connection()
